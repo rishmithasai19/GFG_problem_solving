1fa93d6b50ffd6302599ca2b30151bbcc549e538
rishmithasai19 <247y1a0595@mlritm.ac.in>
2026-07-09 20:34:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Taking Input/taking-input.py
@@ -0,0 +1,14 @@
+import math
+
+# s to store string
+# n to store integer
+# f to store float
+# ff  # To Store floor of float variable f
+
+# code here
+s= input()
+n= int(input())
+ff= int(float(input()))
+print(s)
+print(n)
+print(ff)
